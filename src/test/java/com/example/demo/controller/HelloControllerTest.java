1db6d48ed5d3855800607091045831d0cfc3594c
Juyoung Kim <kjyoung153@hotmail.com>
2024-05-24 14:25:20 +0900

    feat: test code learning

---
+++ src/test/java/com/example/demo/controller/HelloControllerTest.java
@@ -0,0 +1,33 @@
+package com.example.demo.controller;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(controllers = HelloController.class)
+public class HelloControllerTest {
+	
+	@Autowired
+	private MockMvc mvc;
+	
+	@Test
+	public void return_hello() throws Exception {
+		String hello = "hello";
+		
+		mvc.perform(get("/hello"))
+				.andExpect(status().isOk())
+				.andExpect(content().string(hello));
+		
+	}
+	
+	
+
+}
